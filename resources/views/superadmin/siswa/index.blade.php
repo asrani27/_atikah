304a23f2bc999a8e16411ced8a366150c2813810
asrani <asrani.27@gmail.com>
2025-05-13 08:29:46 +0800

    f

--- resources/views/superadmin/siswa/index.blade.php
+++ resources/views/superadmin/siswa/index.blade.php
@@ -5,10 +5,10 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Data pegawai</h3>
+                <h3 class="card-title">Data siswa</h3>
 
                 <div class="card-tools">
-                    <a href="/superadmin/pegawai/add" class='btn btn-sm btn-info'>Tambah Data</a>
+                    <a href="/superadmin/siswa/add" class='btn btn-sm btn-info'>Tambah Data</a>
                 </div>
             </div>
             <!-- /.card-header -->
@@ -17,11 +17,14 @@
                     <thead class="bg-info">
                         <tr>
                             <th>No</th>
-                            <th>NIP/NIK</th>
+                            <th>NIS</th>
                             <th>Nama</th>
-                            <th>Jkel</th>
+                            <th>Tempat Lahir</th>
                             <th>Tanggal Lahir</th>
-                            <th>Jabatan</th>
+                            <th>Jkel</th>
+                            <th>Telp</th>
+                            <th>Nama Ayah</th>
+                            <th>Nama ibu</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
@@ -29,16 +32,18 @@
                         @foreach ($data as $key => $item)
                         <tr style="font-size:14px">
                             <td>{{$key + 1}}</td>
-                            <td>{{$item->nip}}</td>
+                            <td>{{$item->nis}}</td>
                             <td>{{$item->nama}}</td>
-                            <td>{{$item->jkel}}</td>
-                            <td>{{\Carbon\Carbon::parse($item->tgl_lahir)->format('d M Y')}}</td>
-                            <td>{{$item->jabatan}}</td>
+                            <td>{{$item->tempat_lahir}}</td>
+                            <td>{{\Carbon\Carbon::parse($item->tanggal_lahir)->format('d M Y')}}</td>
+                            <td>{{$item->jkel == 'L' ? 'laki-laki': 'perempuan'}}</td>
+                            <td>{{$item->telp}}</td>
+                            <td>{{$item->ayah}}</td>
+                            <td>{{$item->ibu}}</td>
                             <td class="text-right">
-
-                                <a href="/superadmin/pegawai/edit/{{$item->id}}" class="btn btn-sm btn-success"><i
+                                <a href="/superadmin/siswa/edit/{{$item->id}}" class="btn btn-sm btn-success"><i
                                         class="fa fa-edit"></i></a>
-                                <a href="/superadmin/pegawai/delete/{{$item->id}}" class="btn btn-sm btn-danger"
+                                <a href="/superadmin/siswa/delete/{{$item->id}}" class="btn btn-sm btn-danger"
                                     onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i></a>
                             </td>
                         </tr>
